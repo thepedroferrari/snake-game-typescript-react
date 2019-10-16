fb994b3a5efc5441fa279505b55175c1cedc6949
Pedro Ferrari <pedro@pedroferrari.com>
2019-10-17 01:36:41 +0200

    Draw canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import './App.css'
 
-interface Props {
-  canvas: HTMLCanvasElement
-  context: CanvasRenderingContext2D
+interface Position {
+  x: number
+  y: number
 }
-const App: React.FC<Props> = () => {
-  return <></>
+
+const HOOK_SVG = 'M0 0h290v290H0V0z'
+const HOOK_PATH = new Path2D(HOOK_SVG)
+const SCALE = 2
+const OFFSET = 80
+
+function draw(ctx: CanvasRenderingContext2D, location: Position) {
+  ctx.fillStyle = 'deepskyblue'
+  ctx.shadowColor = 'dodgerblue'
+  ctx.shadowBlur = 20
+  ctx.save()
+  ctx.scale(SCALE, SCALE)
+  ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET)
+  ctx.fill(HOOK_PATH)
+  ctx.restore()
+}
+
+const App: React.FC = () => {
+  const canvasRef = React.useRef(null)
+  return (
+    <canvas
+      ref={canvasRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onClick={e => {
+        const canvas: HTMLCanvasElement = canvasRef.current!
+        const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!
+        draw(ctx, { x: e.clientX, y: e.clientY })
+      }}
+    />
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,5 @@ import './index.css'
 import App from './App'
 
 const root: HTMLElement = document.getElementById('root')!
-const canvas: HTMLCanvasElement = document.createElement('canvas')
-const context: CanvasRenderingContext2D = canvas.getContext('2d')!
 
-ReactDOM.render(<App canvas={canvas} context={context} />, root)
+ReactDOM.render(<App />, root)
